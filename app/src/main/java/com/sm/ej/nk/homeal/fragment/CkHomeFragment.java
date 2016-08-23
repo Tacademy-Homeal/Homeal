0fd6911b697f396e751cda8f21e0827529fc9705
Song Min <96s0907@naver.com>
2016-08-23 17:49:34 +0900

    zi

--- app/src/main/java/com/sm/ej/nk/homeal/fragment/CkHomeFragment.java
+++ app/src/main/java/com/sm/ej/nk/homeal/fragment/CkHomeFragment.java
@@ -1,28 +1,33 @@
 package com.sm.ej.nk.homeal.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.sm.ej.nk.homeal.MapActivity;
+import com.sm.ej.nk.homeal.MenuAddActivity;
 import com.sm.ej.nk.homeal.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CkHomeFragment extends Fragment {
 
-    public static CkHomeFragment createInstance(){
-        final CkHomeFragment pageFragment = new CkHomeFragment();
-        final Bundle bundle = new Bundle();
-        pageFragment.setArguments(bundle);
-        return pageFragment;
-    }
+    @BindView(R.id.btn_ck_home_map)
+    Button btnMap;
+
+    @BindView(R.id.btn_ck_home_menu)
+    Button btnMenu;
 
     public CkHomeFragment() {
-        // Required empty public constructor
     }
 
 
@@ -30,7 +35,32 @@ public class CkHomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_et_home, container, false);
+        View v =  inflater.inflate(R.layout.fragment_et_home, container, false);
+        ButterKnife.bind(this, v);
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent  = new Intent(getActivity(), MenuAddActivity.class);
+                startActivity(intent);
+            }
+        });
+        return v;
+    }
+
+    public static CkHomeFragment createInstance(){
+        final CkHomeFragment pageFragment = new CkHomeFragment();
+        final Bundle bundle = new Bundle();
+        pageFragment.setArguments(bundle);
+        return pageFragment;
     }
 
 }
