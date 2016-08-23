a193f6af5c31fb9ca7da20fca23ab4f99601e5bf
Song Min <96s0907@naver.com>
2016-08-23 18:01:50 +0900

    zi

--- app/src/main/java/com/sm/ej/nk/homeal/fragment/CkHomeFragment.java
+++ app/src/main/java/com/sm/ej/nk/homeal/fragment/CkHomeFragment.java
@@ -4,6 +4,7 @@ package com.sm.ej.nk.homeal.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,6 @@ import com.sm.ej.nk.homeal.MapActivity;
 import com.sm.ej.nk.homeal.MenuAddActivity;
 import com.sm.ej.nk.homeal.R;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -21,10 +21,8 @@ import butterknife.ButterKnife;
  */
 public class CkHomeFragment extends Fragment {
 
-    @BindView(R.id.btn_ck_home_map)
     Button btnMap;
 
-    @BindView(R.id.btn_ck_home_menu)
     Button btnMenu;
 
     public CkHomeFragment() {
@@ -37,7 +35,10 @@ public class CkHomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_et_home, container, false);
         ButterKnife.bind(this, v);
+        Log.e("ssong","ddd");
 
+        btnMap = (Button)v.findViewById(R.id.btn_ck_home_map);
+        btnMenu = (Button)v.findViewById(R.id.btn_ck_home_menu);
         btnMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
