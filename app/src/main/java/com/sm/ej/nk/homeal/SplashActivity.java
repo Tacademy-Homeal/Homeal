21d5177105afbc13cb488389c579079321fb51af
Song Min <96s0907@naver.com>
2016-08-23 17:30:42 +0900

    zi

--- app/src/main/java/com/sm/ej/nk/homeal/SplashActivity.java
+++ app/src/main/java/com/sm/ej/nk/homeal/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.sm.ej.nk.homeal;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        moveLoginActivity();
+    }
+
+    Handler mHandler = new Handler(Looper.getMainLooper());
+    private void moveLoginActivity(){
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 2000);
     }
 }
