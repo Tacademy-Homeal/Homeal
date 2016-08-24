16048b141e942c365278e9ec327ec0c982c0cd6b
Eun Ji <kwakeunjea@naver.com>
2016-08-24 14:31:47 +0900

    eunji

--- app/src/main/java/com/sm/ej/nk/homeal/SettingActivity.java
+++ app/src/main/java/com/sm/ej/nk/homeal/SettingActivity.java
@@ -3,15 +3,11 @@ package com.sm.ej.nk.homeal;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class SettingActivity extends AppCompatActivity {
-    @BindView(R.id.btn_setting_advice)
-    Button btnAdvice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
