d492fb1d078fd15f4d35beb5bc93e55b816bbdae
namgil <tompx@naver.com>
2016-08-25 17:26:58 +0900

    InfoCkDetail  toolbar add

--- app/src/main/java/com/sm/ej/nk/homeal/ChattingActivity.java
+++ app/src/main/java/com/sm/ej/nk/homeal/ChattingActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 
 public class ChattingActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
