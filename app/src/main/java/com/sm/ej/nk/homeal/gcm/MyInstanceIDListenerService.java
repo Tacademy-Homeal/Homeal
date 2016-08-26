087df8e43a44f048fb9420545ef17b977546d056
namgil <tompx@naver.com>
2016-08-26 17:43:56 +0900

    add relevant data

---
+++ app/src/main/java/com/sm/ej/nk/homeal/gcm/MyInstanceIDListenerService.java
@@ -0,0 +1,27 @@
+package com.sm.ej.nk.homeal.gcm;
+
+import android.content.Intent;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+/**
+ * Created by Tacademy on 2016-08-26.
+ */
+public class MyInstanceIDListenerService extends InstanceIDListenerService {
+
+    private static final String TAG = "MyInstanceIDLS";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. This call is initiated by the
+     * InstanceID provider.
+     */
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Fetch updated Instance ID token and notify our app's server of any changes (if applicable).
+        Intent intent = new Intent(this, RegistrationIntentService.class);
+        startService(intent);
+    }
+    // [END refresh_token]
+}
---
+++ app/src/main/java/com/sm/ej/nk/homeal/gcm/RegistrationIntentService.java
@@ -0,0 +1,63 @@
+package com.sm.ej.nk.homeal.gcm;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GcmPubSub;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+import com.sm.ej.nk.homeal.R;
+import com.sm.ej.nk.homeal.manager.PropertyManager;
+
+import java.io.IOException;
+
+/**
+ * Created by Tacademy on 2016-08-26.
+ */
+public class RegistrationIntentService  extends IntentService {
+
+
+    private static final String TAG = "RegIntentService";
+    private static final String[] TOPICS = {"global"};
+
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+    public RegistrationIntentService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        try {
+            InstanceID instanceID = InstanceID.getInstance(this);
+            String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
+                    GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+            PropertyManager.getInstance().setRegistrationToken(token);
+            subscribeTopics(token);
+        } catch (Exception e) {
+            Log.d(TAG, "Failed to complete token refresh", e);
+        }
+        Intent registrationComplete = new Intent(REGISTRATION_COMPLETE);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
+    }
+
+    /**
+     * Subscribe to any GCM topics of interest, as defined by the TOPICS constant.
+     *
+     * @param token GCM token
+     * @throws IOException if unable to reach the GCM PubSub service
+     */
+    // [START subscribe_topics]
+    private void subscribeTopics(String token) throws IOException {
+        GcmPubSub pubSub = GcmPubSub.getInstance(this);
+        for (String topic : TOPICS) {
+            pubSub.subscribe(token, "/topics/" + topic, null);
+        }
+    }
+    // [END subscribe_topics]
+
+
+}
