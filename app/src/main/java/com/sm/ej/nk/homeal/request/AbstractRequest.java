3f8528661f804c0efc48deec0d4d92ed2b26f426
Song Min <96s0907@naver.com>
2016-08-26 13:11:02 +0900

    ssongmin

--- app/src/main/java/com/sm/ej/nk/homeal/request/AbstractRequest.java
+++ app/src/main/java/com/sm/ej/nk/homeal/request/AbstractRequest.java
@@ -1,7 +1,58 @@
 package com.sm.ej.nk.homeal.request;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.sm.ej.nk.homeal.data.NetworkResult;
+import com.sm.ej.nk.homeal.data.NetworkResultTemp;
+import com.sm.ej.nk.homeal.manager.NetworkRequest;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+import okhttp3.HttpUrl;
+import okhttp3.ResponseBody;
+
 /**
  * Created by Tacademy on 2016-08-26.
  */
-public class AbstractRequest {
+public abstract class AbstractRequest<T> extends NetworkRequest<T> {
+    private static final String DUMMY_URL = "ec2-52-78-131-245.ap-northeast-2.compute.amazonaws.com";
+
+    protected HttpUrl.Builder getBaseHttpsUrlBuilder() {
+        HttpUrl.Builder builder = new HttpUrl.Builder();
+        builder.scheme("https");
+        builder.host(DUMMY_URL);
+        return builder;
+    }
+
+    protected HttpUrl.Builder getBaseHttpUrlBuilder() {
+        HttpUrl.Builder builder = new HttpUrl.Builder();
+        builder.scheme("http");
+        builder.host(DUMMY_URL);
+        return builder;
+    }
+
+    @Override
+    protected T parse(ResponseBody body) throws IOException {
+        String text = body.string();
+        Gson gson = new Gson();
+        NetworkResultTemp temp = gson.fromJson(text, NetworkResultTemp.class);
+        if (temp.getCode() == 1) {
+            T result = gson.fromJson(text, getType());
+            return result;
+        } else if (temp.getCode() == 2) {
+            Type type = new TypeToken<NetworkResult<String>>(){}.getType();
+            NetworkResult<String> result = gson.fromJson(text, type);
+            throw new IOException(result.getResult());
+        } else {
+            T result = gson.fromJson(text, getType(temp.getCode()));
+            return result;
+        }
+    }
+
+    protected Type getType(int code) {
+        return getType();
+    }
+
+    protected abstract Type getType();
 }
