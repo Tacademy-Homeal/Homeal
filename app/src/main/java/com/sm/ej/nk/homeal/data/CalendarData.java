868ad1884da60e855fabd839000ac88288891660
Song Min <96s0907@naver.com>
2016-08-26 10:01:34 +0900

    ssongmin calendar

---
+++ app/src/main/java/com/sm/ej/nk/homeal/data/CalendarData.java
@@ -0,0 +1,14 @@
+package com.sm.ej.nk.homeal.data;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Tacademy on 2016-08-26.
+ */
+public class CalendarData {
+    public int year;
+    public int month;
+    public int weekOfMonth;
+    public int weekOfYear;
+    public ArrayList<CalendarItem> days = new ArrayList<>();
+}
