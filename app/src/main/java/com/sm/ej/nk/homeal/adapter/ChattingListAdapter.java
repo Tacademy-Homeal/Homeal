a01c53827e9d129b7f62d11890956df1ddaad1b0
namgil <tompx@naver.com>
2016-08-29 17:59:37 +0900

    some change

--- app/src/main/java/com/sm/ej/nk/homeal/adapter/ChattingListAdapter.java
+++ app/src/main/java/com/sm/ej/nk/homeal/adapter/ChattingListAdapter.java
@@ -1,65 +1,53 @@
 package com.sm.ej.nk.homeal.adapter;
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.sm.ej.nk.homeal.ChattingActivity;
 import com.sm.ej.nk.homeal.R;
+import com.sm.ej.nk.homeal.data.ChatContract;
 import com.sm.ej.nk.homeal.viewholder.ChattingListViewHolder;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.sm.ej.nk.homeal.viewholder.ChattingSendViewHolder;
 
 /**
  * Created by Tacademy on 2016-08-25.
  */
 public class ChattingListAdapter extends RecyclerView.Adapter<ChattingListViewHolder>{
 
-    Context context;
-    Intent intent;
-    List<Drawable> items = new ArrayList<>();
+    Cursor cursor;
 
-    public void add(Drawable foodview){
-        items.add(foodview);
-        notifyDataSetChanged();
-    }
+    CursorAdapter mCursorAdapter;
+    Context mContext;
+    private String[] from;
+    private int[] to;
 
     @Override
     public int getItemCount() {
-        return items.size();
-    }
-
-    @Override
-    public void onBindViewHolder(ChattingListViewHolder holder, int position) {
-        holder.setImage(items.get(position));
-
-        holder.getChattingListView().setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(intent);
-            }
-        });
-
-        holder.getLastConversation().setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(intent);
-            }
-        });
+        if (cursor == null) return 0;
+        return cursor.getCount();
     }
 
     @Override
     public ChattingListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_chattinglist, parent, false);
-        context = parent.getContext();
-        intent= new Intent(parent.getContext(), ChattingActivity.class);
         return new ChattingListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ChattingListViewHolder holder, int position) {
+
+
+        cursor.moveToPosition(position);
+        switch (holder.getItemViewType()) {
+            case VIEW_TYPE_SEND : {
+                ChattingSendViewHolder svh = (ChattingSendViewHolder)holder;
+                String message = cursor.getString(cursor.getColumnIndex(ChatContract.ChatMessage.COLUMN_MESSAGE));
+                svh.setMessage(message);
+                break;
+            }
     }
 }
