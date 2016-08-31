61159166c75b61ce9aaa24a10e8dedaf1787a9f7
namgil <tompx@naver.com>
2016-08-31 17:07:30 +0900

    CK reserve page modify

--- app/src/main/java/com/sm/ej/nk/homeal/fragment/CkReserveFragment.java
+++ app/src/main/java/com/sm/ej/nk/homeal/fragment/CkReserveFragment.java
@@ -1,20 +1,16 @@
 package com.sm.ej.nk.homeal.fragment;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.sm.ej.nk.homeal.CkWriteReViewActivity;
 import com.sm.ej.nk.homeal.R;
 import com.sm.ej.nk.homeal.adapter.CkReserveAdapter;
 import com.sm.ej.nk.homeal.data.CkReserveData;
@@ -31,6 +27,11 @@ public class CkReserveFragment extends Fragment {
     @BindView(R.id.rv_ck_reserve)
     RecyclerView CkReserveView;
     CkReserveAdapter mAdapter;
+
+    private static final int TYPE_QUEST = 0;
+    private static final int TYPE_QUEST_COMPLETE = 1;
+    private static final int TYPE_EAT_END = 2;
+    private static final int TYPE_END = 3;
 
     public static CkReserveFragment createInstance() {
         final CkReserveFragment pageFragment = new CkReserveFragment();
@@ -54,72 +55,34 @@ public class CkReserveFragment extends Fragment {
 
         CkReserveView.setAdapter(mAdapter);
         CkReserveView.setLayoutManager(manager);
+
+
         AlertDialog dialog;
 
         initData();
-
-        mAdapter.setOnAdapterItemClickListener(new CkReserveAdapter.OnAdapterItemClickLIstener() {
-            @Override
-            public void onAdapterItemClick(View view, CkReserveData ckReserveData, int position) {
-                //  showdialog();
-                Toast.makeText(getContext(), "예약이 승인되었습니다.", Toast.LENGTH_SHORT).show();
-            }
-
-        });
-        mAdapter.setOndisAdapterItemClickListener(new CkReserveAdapter.OndisAdapterItemClickLIstener() {
-            @Override
-            public void ondisAdapterItemClick(View view, CkReserveData data, int position) {
-                Toast.makeText(getContext(), "예약이 거절되었습니다.", Toast.LENGTH_SHORT).show();
-            }
-        });
-        mAdapter.setOnreviewAdapterItemClickListener(new CkReserveAdapter.OnreviewAdapterItemClickLIstener() {
-            @Override
-            public void onreviewAdapterItemClick(View view, CkReserveData data, int position) {
-                startActivity(new Intent(getContext(), CkWriteReViewActivity.class));
-
-            }
-        });
         return view;
     }
-
-//    private void showdialog() {
-//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//        builder.setIcon(android.R.drawable.ic_dialog_alert);
-//        selectedPosition = 2;
-//        builder.setSingleChoiceItems(items, 2, new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialogInterface, int i) {
-//                selectedPosition = i;
-//            }
-//        });
-//        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialogInterface, int i) {
-//                if (selectedPosition != -1) {
-//                    Toast.makeText(MainActivity.this, "selected : " + items[selectedPosition], Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
-//
-//        builder.create().show();
-//    }
-
-//}
-
 
     private void initData() {
         Random r = new Random();
         for (int i = 0; i < 20; i++) {
             CkReserveData data = new CkReserveData();
-            data.setFoodname("foodname " + i);
-            data.setReservedate("date" + i);
-            data.setReserveperson("person" + i);
-            data.setEtname("ckname" + i);
-            data.setReservestate("state" + i);
-            data.setEtpicture(ContextCompat.getDrawable(getContext(), R.mipmap.ic_launcher));
-            data.setBtnagree("승인");
-            data.setBtndisagree("거절");
-            data.setBtnreviewwrite("후기 작성");
+            data.setFoodName("Noodle");
+            data.setReservePerson("Namgil");
+            data.setEtName("Lee");
+            data.setReserveDate("2010.10.5");
+
+            if(i < 10) {
+                data.setReserveState(TYPE_QUEST);
+            }else if(i < 13 || i < 10){
+                data.setReserveState(TYPE_QUEST_COMPLETE);
+            }else if(i< 17 || i > 13){
+                data.setReserveState(TYPE_EAT_END);
+            }else{
+                data.setReserveState(TYPE_END);
+            }
+
+            data.setEtPictureUrl("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDLcDeVw9X8oxxP6EpKmgcBi5iG9SWnxThw0WHhg2DevUyEOk9");
             mAdapter.add(data);
         }
 
