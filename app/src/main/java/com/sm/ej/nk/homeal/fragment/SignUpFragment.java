bda78091f96d85fb9cd04bb677f13ff13cb90554
Nam gil <tompx@naver.com>
2016-09-05 14:53:19 +0900

    icon change

--- app/src/main/java/com/sm/ej/nk/homeal/fragment/SignUpFragment.java
+++ app/src/main/java/com/sm/ej/nk/homeal/fragment/SignUpFragment.java
@@ -1,24 +1,22 @@
 package com.sm.ej.nk.homeal.fragment;
 
 
-import android.app.Activity;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.sm.ej.nk.homeal.LoginActivity;
 import com.sm.ej.nk.homeal.R;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,11 +31,22 @@ public class SignUpFragment extends Fragment {
     @BindView(R.id.sf_ok)
     Button sf_ok;
 
-    @BindView(R.id.edit_sign_up)
-    EditText editText;
+    @BindView(R.id.edit_frist_name)
+    EditText edit_frist_name;
 
-    @BindView(R.id.image_sign_up)
-    ImageView image;
+    @BindView(R.id.edit_second_name)
+    EditText edit_second_name;
+
+    @BindView(R.id.singUp_spinner_ck_year)
+    Spinner spinner_year;
+
+    @BindView(R.id.singUp_spinner_ck_month)
+    Spinner spinner_month;
+
+    @BindView(R.id.singUp_spinner_ck_day)
+    Spinner spinner_day;
+
+
 
     public SignUpFragment() {
         // Required empty public constructor
@@ -51,16 +60,40 @@ public class SignUpFragment extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_sign_up, container, false);
         ButterKnife.bind(this, view);
 
+
+
+        ArrayList<String> monthList = new ArrayList<>();
+        for (int month = 1; month < 13; month++) {
+            monthList.add(String.valueOf(month));
+        }
+        ArrayAdapter<String> monthAdatper = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, monthList);
+        spinner_month.setAdapter(monthAdatper);
+
+        ArrayList<String> dayList = new ArrayList<>();
+        for (int day = 1; day < 32; day++) {
+            dayList.add(String.valueOf(day));
+        }
+        ArrayAdapter<String> dayAdatper = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, dayList);
+        spinner_day.setAdapter(dayAdatper);
+
+        ArrayList<String> yearList = new ArrayList<>();
+        for (int year = 1930; year < 2031; year++) {
+            yearList.add(String.valueOf(year));
+        }
+        ArrayAdapter<String> yearAdatper = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, yearList);
+        spinner_year.setAdapter(yearAdatper);
+
         return view;
     }
 
     @OnClick(R.id.sf_ok)
     public void onSingUpOk(){
-        if(!TextUtils.isEmpty(editText.getText().toString())){
-            ((LoginActivity)getActivity()).moveMainActivity();
-        }else{
-            showDialog();
-        }
+        ((LoginActivity)getActivity()).moveMainActivity();
+
+//        if(!TextUtils.isEmpty(editText.getText().toString())){
+//        }else{
+//            showDialog();
+//        }
     }
     private void showDialog(){
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -76,21 +109,6 @@ public class SignUpFragment extends Fragment {
 
     private static final int GET_IMAGE = 1;
 
-    @OnClick(R.id.image_sign_up)
-    public void onGallery(){
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        intent.setType("image/*");
-        startActivityForResult(intent, GET_IMAGE);
-    }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == GET_IMAGE){
-            if(resultCode == Activity.RESULT_OK){
-                Uri uri = data.getData();
-                image.setImageURI(uri);
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
+
 }
